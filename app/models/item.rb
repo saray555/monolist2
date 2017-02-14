847039c86dd29b2b84b19be05d5d13ffccef10e5
Noriko Ohnuki <nnorky@gmail.com>
2017-02-14 10:14:07 +0000

    updated

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ActiveRecord::Base
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+  
+  ### Need to check ###
+  has_many :wants, class_name: "Want_user", foreign_key: "user_id", dependent: :destroy
+  has_many :want_users , through: :wants, source: :user
+  has_many :haves, class_name: "Have_user", foreign_key: "user_id", dependent: :destroy
+  has_many :have_users , through: :haves, source: :user
+  
 end
